602e2259f68e43c2ec330c137b9fe4ddd0c5d725
Camilla Dahlen <camidahlen@gmail.com>
2022-11-29 15:53:57 +0100

    made compilation fail

--- src/main/java/no/shoppifly/ShoppingCartController.java
+++ src/main/java/no/shoppifly/ShoppingCartController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController()
-public class ShoppingCartController {
+public class ShoppingCartController
 
     @Autowired
     private final CartService cartService;
